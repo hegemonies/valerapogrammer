7429629801a9afb678029854b68b32ab78f41389
hegemonies <hegemonies@yandex.ru>
2019-10-13 00:01:09 +0700

    Add rspec to test Valera properties and litle bit fix Valera

--- src/ruby/valera.rb
+++ src/ruby/valera.rb
@@ -1,4 +1,29 @@
 
 class Valera
+  attr_reader :fatigue, :money
 
+  def initialize(fatigue, money)
+    @fatigue = fatigue
+    @money = money
+  end
+
+  def add_fatigue(number)
+    if @fatigue + number < 0
+      return @fatigue = 0
+    end
+
+    if @fatigue + number > 100
+      return @fatigue = 100
+    end
+
+    @fatigue += number
+  end
+
+  def add_money(number)
+    if @money + number < 0
+      return @money = 0
+    end
+
+    @money += number
+  end
 end
--- src/spec/valera_spec.rb
+++ src/spec/valera_spec.rb
@@ -1,17 +1,47 @@
 require 'rspec'
+require_relative '../../src/ruby/valera'
 
-describe 'Valera' do
-  before do
-    # Do nothing
+describe Valera do
+  describe '#add_fatigue' do
+    subject { described_class.new(0, 0) }
+
+    context 'regular add' do
+      it 'add 50' do
+        subject.add_fatigue 50
+        expect(subject.fatigue).to eql(50)
+      end
+    end
+
+    context 'over add' do
+      it 'add 110' do
+        subject.add_fatigue 110
+        expect(subject.fatigue).to eql(100)
+      end
+    end
+
+    context 'low add' do
+      it 'minus 50' do
+        subject.add_fatigue -50
+        expect(subject.fatigue).to eql(0)
+      end
+    end
   end
 
-  after do
-    # Do nothing
-  end
+  describe '#add_fatigue' do
+    subject { described_class.new(0, 0) }
 
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+    context 'just add' do
+      it 'add 50' do
+        subject.add_money 50
+        expect(subject.money).to eql(50)
+      end
+    end
+
+    context 'just minus' do
+      it 'minus 50' do
+        subject.add_money -50
+        expect(subject.money).to eql(0)
+      end
     end
   end
 end
