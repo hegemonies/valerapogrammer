f137bad02c75641477c4b8bbecf8a8a7279cd8ba
AlexanderSenchenko <zayzsacha1998@gmail.com>
2019-10-13 15:01:16 +0700

    2 part

---
+++ src/spec/valera_field_health_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../../src/ruby/valera'
+
+describe Valera do
+  describe '#add_health' do
+    subject { described_class.new(0, 0, 0, 0, 0) }
+
+    context 'regular add' do
+      it 'add 50' do
+        subject.add_health(50)
+        expect(subject.health).to eql(50)
+      end
+    end
+
+    context 'over add' do
+      it 'add 110' do
+        subject.add_health(110)
+        expect(subject.health).to eql(100)
+      end
+    end
+
+    context 'low add' do
+      it 'minus 50' do
+        subject.add_health(-50)
+        expect(subject.health).to eql(0)
+      end
+    end
+  end
+end
